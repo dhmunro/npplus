359579462c09a1a91934f56ba2db532a68a739bb
dhmunro <dhmunro@users.sourceforge.net>
2016-04-09 17:24:36 -0700

    changed interactive to use "from npplus.interactive import *"

--- npplus/interactive.py
+++ npplus/interactive.py
@@ -1,44 +1,36 @@
 """Import most names in npplus for interactive use.
 
-import npplus.interactive
+from npplus.interactive import *
 
 This module should be imported only from PYTHONSTARTUP, except for
-similar interactive.py modules in other packages, which should use
-the npplus.interactive.import_all_to_main function.
+similar interactive.py modules in other packages.
 """
-
-import __main__
-def import_all_to_main(*modules):
-    """same as typing 'from module import *' in interactive session"""
-    for m in modules:
-        names = getattr(m, '__all__', None)
-        if names is None:
-            names = [k for k in m.__dict__.keys() if not k.startswith('_')]
-        for nm in names:
-            setattr(__main__, nm, getattr(m, nm))
 
 # sys and os too common to have to import them
 import sys, os
-__main__.sys, __main__.os = sys, os
 
 # duplicate the pylab numpy imports for interactive use
 import numpy as np
 import numpy.ma as ma
-__main__.np, __main__.ma = np, ma
-import_all_to_main(np, np.fft, np.random, np.linalg)
+from numpy import *
+from numpy.fft import *
+from numpy.random import *
+from numpy.linalg import *
 # fix clobbered datetime and bytes (np.random.bytes)
 # note: modern numpy does not clobber datetime, but this is harmless
 import datetime
-__main__.datetime, __main__.bytes = datetime, __builtins__['bytes']
+bytes = __builtins__['bytes']
+
 # force sane SIGFPE error handling
 np.seterr(divide='raise', over='raise', invalid='raise')
 
 # give interpreted access to npplus modules
-from . import yorlike, pwpoly, pcwise
-import_all_to_main(yorlike, pwpoly, pcwise)
+from .yorlike import *
+from .pwpoly import *
+from .pcwise import *
 
 # implement deprecated execfile for python3
-if sys.version_info >= (3,0) and not hasattr(__main__, 'execfile'):
+if sys.version_info >= (3,0):
     if sys.version_info >= (3,4):
         from importlib import reload
     else:
@@ -49,7 +41,6 @@ if sys.version_info >= (3,0) and not hasattr(__main__, 'execfile'):
         with open(name) as f:
             code = compile(f.read(), name, 'exec')
             eval("exec(code, *args)")  # evade syntax error in python2
-    __main__.execfile, __main__.reload = execfile, reload
 
 # implement extended reload for development reloadx-pdb-edit cycle
 def reloadx(module):
@@ -67,5 +58,7 @@ def reloadx(module):
     # be sure to recreate any objects constructed from modified classes
     # loop debug, edit, reloadx
     """
-    import_all_to_main(reload(module))
-__main__.reloadx = reloadx
+    reload(module)
+    import __main__
+    for nm in vars(module):
+        setattr(__main__, nm, getattr(module, nm))
