5c8439e5fbad71a246dde353a3295ac50cb0fc8f
dhmunro <dhmunro@users.sourceforge.net>
2016-07-05 21:10:10 -0700

    debugged fermi.py

--- npplus/fermi.py
+++ npplus/fermi.py
@@ -1,15 +1,24 @@
 """Fermi-Dirac integral and inverse of orders -1/2, 1/2, 3/2, 5/2
-Antia, H.M., Aph.J 84, 101-108 (1993)
+
+Notes
+-----
+
+See [1]_ for algorithms.
+
+References
+----------
+
+.. [1] Antia, H.M., Aph.J Supp. 84, pp. 101-108 (1993).
 
 --------
 """
 
 # fermi integral related to polylogarithm:  F_j(x) = -Li_j+1(-exp(x))
 
-__all__ = ['fdm12', 'fd12', 'fd32', 'fd52']
+__all__ = ['fdm12', 'fd12', 'fd32', 'fd52', 'ifdm12', 'ifd12', 'ifd32', 'ifd52']
 
 from functools import wraps
-from numpy import asfarray, zeros_like, exp, polyval, sqrt
+from numpy import asfarray, zeros_like, exp, sqrt, log
 
 
 # The algorithm for the various orders is almost identical,
@@ -22,21 +31,29 @@ def _fd_integ(anum, aden, bnum, bden):
         def fdtemplate(x):
             x = asfarray(x)
             shape, x = x.shape, x.ravel()
-            mask = x <= 2.
+            mask = x < 2.
             y = zeros_like(x)
             z = x[mask]
             if z.size:
                 z = exp(z)
-                y[mask] = (z * polyval(anum, z) / polyval(aden, z))
+                y[mask] = z * _peval(anum, z) / _peval(aden, z)
             mask = ~mask
             x = x[mask]
             if x.size:
                 z = 1. / (x * x)
                 x = sqrt(x) * f(x)  # note only use of original f
-                y[mask] = (x * polyval(bnum, z) / polyval(bden, z))
+                y[mask] = x * _peval(bnum, z) / _peval(bden, z)
             return y.reshape(shape)
         return fdtemplate
     return fdwrapper
+
+
+def _peval(c, x):
+    p = c[0]*x + c[1]
+    for a in c[2:]:
+        p *= x
+        p += a
+    return p
 
 
 @_fd_integ((1.0, 1.98276889924768e3, 1.14980998186874e5, 1.83696370756153e6,
@@ -86,7 +103,7 @@ def fdm12(x):
             1.92040136756592e-3, 5.02360015186394e-5, 6.96888634549649e-7,
             5.62152894375277e-9, 2.45745452167585e-11, 7.28067571760518e-14))
 def fd12(x):
-    """Fermi-Dirac integral of order -1/2.
+    """Fermi-Dirac integral of order +1/2.
 
     Parameters
     ----------
@@ -115,7 +132,7 @@ def fd12(x):
             2.04569943213216e-4, 3.84703231868724e-6, 3.94452010378723e-8,
             2.10699282897576e-10, 7.01131732871184e-13))
 def fd32(x):
-    """Fermi-Dirac integral of order -1/2.
+    """Fermi-Dirac integral of order +3/2.
 
     Parameters
     ----------
@@ -144,7 +161,7 @@ def fd32(x):
             8.09451165406274e-5, 1.12919616415947e-6, 7.68215783076936e-9,
             2.94933476646033e-11))
 def fd52(x):
-    """Fermi-Dirac integral of order -1/2.
+    """Fermi-Dirac integral of order +5/2.
 
     Parameters
     ----------
@@ -157,3 +174,117 @@ def fd52(x):
         accurate to about 1e-12.
     """
     return x*x*x
+
+
+def _fd_integ(anum, aden, bnum, bden):
+    def fdwrapper(f):
+        @wraps(f)
+        def fdtemplate(x):
+            x = asfarray(x)
+            shape, x = x.shape, x.ravel()
+            mask = x < 4.
+            y = zeros_like(x)
+            z = x[mask]
+            if z.size:
+                y[mask] = log(z * _peval(anum, z) / _peval(aden, z))
+            mask = ~mask
+            x = x[mask]
+            if x.size:
+                z = f(x)  # original f just returns x scaling
+                y[mask] = _peval(bnum, z) / (z * _peval(bden, z))
+            return y.reshape(shape)
+        return fdtemplate
+    return fdwrapper
+
+
+@_fd_integ((1.0, -3.174780572961e1, 4.121170498099e2, -2.805343454951e3,
+            1.001958278442e4, -1.570044577033e4),
+           (-1.008561571363e0, 3.168918168284e1, -4.225615045074e2,
+            3.063252215963e3, -1.274243093149e4, 2.886114034012e4,
+            -2.782831558471e4),
+           (1.0, -9.588603457639e-2, 1.814141021608e-2, -1.169411057416e-3,
+            6.103116850636e-5, -1.437701234283e-6, 2.206779160034e-8),
+           (3.124344749296e0, -3.217372489776e-1, 6.932122275919e-2,
+            -4.601959491394e-3, 2.429627688357e-4, -5.750804196059e-6,
+            8.827116613576e-8))
+def ifdm12(x):
+    """Inverse of Fermi-Dirac integral of order -1/2.
+
+    Parameters
+    ----------
+    x : array_like
+
+    Returns
+    -------
+    y : ndarray
+        x == ``integral[0 to inf]{ dt * t**(-0.5) / (exp(t-y)+1) }``
+        accurate to about 1e-8.
+    """
+    return 1./(x*x)
+
+
+@_fd_integ((1.0, 3.818838129486e1, 6.610132843877e2, 5.702479099336e3,
+            1.999266880833e4),
+           (-1.670718177489e0, 9.130355392717e1, -2.014785161019e3,
+            1.771804140488e4),
+           (1.0, -3.930805454272e-1, -1.285579118012e0, 4.997559426872e-1,
+            -4.262314235106e-1, 7.187946804945e-2, -1.277060388085e-2),
+           (-6.067091689181e-2, -1.145531476975e0, 4.077841975923e-1,
+            -3.299466243260e-1, 5.485432756838e-2, -9.745794806288e-3))
+def ifd12(x):
+    """Inverse of Fermi-Dirac integral of order +1/2.
+
+    Parameters
+    ----------
+    x : array_like
+
+    Returns
+    -------
+    y : ndarray
+        x == ``integral[0 to inf]{ dt * t**0.5 / (exp(t-y)+1) }``
+        accurate to about 1e-8.
+    """
+    return x ** (-2./3.)
+
+
+@_fd_integ((1.0, 2.056296753055e1, 1.125926232897e2, 1.715627994191e2),
+           (3.519268762788e-3, -3.226808804038e-1, 1.167743113540e1,
+            1.193456203021e2, 2.280653583157e2),
+           (1.0, 3.684471177100e-1, -5.951932864088e-1, -4.657944387545e-1,
+            -1.057562799320e-1, -2.183147266896e-2, -6.321828169799e-3),
+           (-1.387107009074e-1, -5.074812565486e-1, -3.407561772612e-1,
+            -7.850001283886e-2, -1.513236504100e-2, -4.381942605018e-3))
+def ifd32(x):
+    """Inverse of Fermi-Dirac integral of order +3/2.
+
+    Parameters
+    ----------
+    x : array_like
+
+    Returns
+    -------
+    y : ndarray
+        x == ``integral[0 to inf]{ dt * t**1.5 / (exp(t-y)+1) }``
+        accurate to about 1e-8.
+    """
+    return x ** (-2./5.)
+
+
+@_fd_integ((1.0, 3.539903493971e1, 2.138969250409e2), (-1.182798726503e-2, 1.067755522895e0, 9.873746988121e1, 7.108545512710e2), (1.0, -1.498867562255e0, 5.495613498630e-1, 5.099038074944e-1, -4.820942898296e-1, 1.315763372315e-1, -3.312041011227e-2), (-3.008504449098e-2, 3.739781456585e-2, -3.847241692193e-1, 5.415026856351e-1, -3.835879295548e-1, 9.198776585252e-2, -2.315515517515e-2))
+def ifd52(x):
+    """Inverse of Fermi-Dirac integral of order +5/2.
+
+    Parameters
+    ----------
+    x : array_like
+
+    Returns
+    -------
+    y : ndarray
+        x == ``integral[0 to inf]{ dt * t**2.5 / (exp(t-y)+1) }``
+        accurate to about 1e-8.
+    """
+    return x ** (-2./7.)
+
+
+del _fd_integ
